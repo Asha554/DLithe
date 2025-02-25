587bcb7f94c213d26d92d835cd1110e3738ec1f3
Asha554 <asha.21ai008@sode-edu.in>
2025-02-25 14:35:06 +0530

    task

---
+++ script.js
@@ -0,0 +1,16 @@
+// Declaring some variables
+let name = "John Doe";
+const age = 30;
+var isStudent = false;
+let hobbies = ["reading", "coding", "traveling"];
+let address = { street: "123 Main St", city: "Anytown", zip: "12345" };
+
+// Printing variables to the console
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
+console.log(address);
+
+// You can also print combined information
+
